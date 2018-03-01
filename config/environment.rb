60b3ad17a8ce5648006690387ebfbd30a131c751
Sean Oughton <seanoughton@gmail.com>
2018-03-01 15:51:50 -0800

    made changes to the require files

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,7 @@
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
+
+require_relative "../lib/sf_bay_area_concerts_cli_app/cli.rb"
+require_relative "../lib/sf_bay_area_concerts_cli_app/scraper.rb"
+require_relative "../lib/sf_bay_area_concerts_cli_app/concert.rb"
