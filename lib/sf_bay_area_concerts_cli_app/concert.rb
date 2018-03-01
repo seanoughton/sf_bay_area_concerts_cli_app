ac326f41c9c9256f0e58694b72d0f5fba93db9ff
Sean Oughton <seanoughton@gmail.com>
2018-03-01 11:51:47 -0800

    Added @@all array to concerts to keep track of concerts. Added functionality that adds a concert to the array when concerts are initialized and also created an #all method to expose the array

--- lib/sf_bay_area_concerts_cli_app/concert.rb
+++ lib/sf_bay_area_concerts_cli_app/concert.rb
@@ -3,6 +3,9 @@ class Concert
 	attr_accessor :url, :artist_name, :location, :date_showtime, :available, :ticket_price, :bio, :artist_website, :where_to_find_music, :facebook, :twitter, :instagram, :youtube
 
 
+	@@all = []
+
+
 	def self.new_concert_from_index_element(url,artist_name,location,date_showtime,available)
 		self.new(url,artist_name,location,date_showtime,available)
 	end
@@ -13,6 +16,11 @@ class Concert
 		@location = location
 		@date_showtime = date_showtime
 		@available = available
+		@@all << self
+	end
+
+	def self.all
+		@@all
 	end
 
 
