a5b9f9efddab5a170ce6387c8c3787294c83ea89
Sean Oughton <seanoughton@gmail.com>
2018-03-01 11:12:49 -0800

    Created a config folder and an environment file that requires all of the necessary gems: nokogiri, open-uri, pry

--- lib/sf_bay_area_concerts_cli_app/cli.rb
+++ lib/sf_bay_area_concerts_cli_app/cli.rb
@@ -5,4 +5,3 @@
 require_relative "scraper.rb" #will do this here for now, but will move this to the environment file
 
 puts Scraper.new.get_all_concerts #creates a new Scraper instance and calls a scraper instance method on it
-
