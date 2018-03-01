27f2a96e26b06a2a3d023cfaa085801217970107
Sean Oughton <seanoughton@gmail.com>
2018-03-01 10:17:42 -0800

    Created Scraper Class and stubbing out basic structure with comments

---
+++ lib/sf_bay_area_concerts_cli_app/scraper.rb
@@ -0,0 +1,15 @@
+class Scraper
+	#create a method that uses Nokogiri  and open uri to return the page as a Nokogiri array - http://apeconcerts.com
+
+	#create a method that gets all of the concerts from the page
+	#each concert has a class of ape-event, mix detail-information
+ 
+
+	#create a method that scrapes each individual concert
+	#needs to have a url for each specific concert
+
+	#create a method that scrapes a specific concert based on a specific concert url
+
+
+
+end
