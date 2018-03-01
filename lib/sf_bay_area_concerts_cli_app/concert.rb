a445ab166f1b8b9625dbbeb66d1e9efb2542a6da
Sean Oughton <seanoughton@gmail.com>
2018-03-01 12:57:04 -0800

    Created a method to detect if the css selector actually returns a nokogiri element. Implement that check for each of the concert additional attributes.

--- lib/sf_bay_area_concerts_cli_app/concert.rb
+++ lib/sf_bay_area_concerts_cli_app/concert.rb
@@ -22,17 +22,43 @@ class Concert
 	def self.all
 		@@all
 	end
+
+	def check_for_nokogiri_object?(selector)
+		true if selector.inspect != "[]"
+	end
 ##
 	def add_additional_attributes
 		doc = Scraper.scrape_specific_concert(self.url)
-		self.bio = doc.css(".bio p")[0..-1].text
-		self.artist_website = doc.css(".more-information.social-icons #website")[0]['href']
-		self.where_to_find_music = doc.css(".more-information.social-icons #music")[0]['href']
-		self.facebook = doc.css(".more-information.social-icons #facebook")[0]['href']
-		self.twitter = doc.css(".more-information.social-icons #twitter")[0]['href']
-		self.instagram = doc.css(".more-information.social-icons #instagram")[0]['href']
-		self.youtube = doc.css(".more-information.social-icons #youtube")[0]['href']
+
+		if check_for_nokogiri_object?(doc.css(".bio p"))
+			self.bio = doc.css(".bio p")[0..-1].text
 		end
+
+
+		if check_for_nokogiri_object?(doc.css(".more-information.social-icons #website"))
+			self.artist_website = doc.css(".more-information.social-icons #website")[0]['href']
+		end
+		
+
+		if check_for_nokogiri_object?(doc.css(".more-information.social-icons #music"))
+			self.where_to_find_music = doc.css(".more-information.social-icons #music")[0]['href']
+		end
+
+		if check_for_nokogiri_object?(doc.css(".more-information.social-icons #facebook"))
+			self.facebook = doc.css(".more-information.social-icons #facebook")[0]['href']
+		end
+
+		if check_for_nokogiri_object?(doc.css(".more-information.social-icons #twitter"))
+			self.twitter = doc.css(".more-information.social-icons #twitter")[0]['href']
+		end
+
+		if check_for_nokogiri_object?(doc.css(".more-information.social-icons #instagram"))
+			self.instagram = doc.css(".more-information.social-icons #instagram")[0]['href']
+		end
+		if check_for_nokogiri_object?(doc.css(".more-information.social-icons #youtube"))
+			self.youtube = doc.css(".more-information.social-icons #youtube")[0]['href']
+		end
+		
 	end
 
 
